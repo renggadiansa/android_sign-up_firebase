7b05b01e189d1bc187a9fb023ebc30af5728ecea
renggadiansa <renggadiansa@gmail.com>
2022-11-15 18:32:43 +0700

    first commit

---
+++ app/src/main/java/com/example/loginregisterfirebase/homePage.kt
@@ -0,0 +1,31 @@
+package com.example.loginregisterfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class homePage : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+
+        findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+
+        findViewById<Button>(R.id.gsigutbtn).setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
