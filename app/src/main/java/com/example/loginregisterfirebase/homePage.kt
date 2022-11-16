dc859bc044de695ac2fbf454a7555173a4c4093e
renggadiansa <renggadiansa@gmail.com>
2022-11-16 11:58:49 +0700

    fixed bug

--- app/src/main/java/com/example/loginregisterfirebase/homePage.kt
+++ app/src/main/java/com/example/loginregisterfirebase/homePage.kt
@@ -1,5 +1,6 @@
 package com.example.loginregisterfirebase
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +13,7 @@ class homePage : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
 
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
